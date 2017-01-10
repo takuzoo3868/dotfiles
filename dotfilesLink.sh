e80ef784de436a4ad8e82d9fd1c40b0deb00a8d3
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-01-11 02:26:13 +0900

    Removed bundles

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,7 +7,7 @@ setup() {
   dotfiles=$HOME/.dotfiles
 
   ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-  ln -s ~/.dotfiles/.vim/ ~/.vim
+
   ln -s ~/.dotfiles/.vimrc ~/.vimrc
   ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 
