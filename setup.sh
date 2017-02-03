90a5b9ea33854f6b6e81f1afc8e18a9bbec4538d
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-02-03 21:25:51 +0900

    update setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 set -u
@@ -88,7 +88,7 @@ setup() {
   sudo pip2 install --upgrade neovim
   sudo pip3 install --upgrade neovim
   symlink "$dotfiles/.config/nvim" "$HOME/.config/nvim"
-  echo -e "<<< [\e[1;32m ok \e[m] neovim & vim"
+  /bin/echo -e "<<< [\e[1;32m ok \e[m] neovim & vim"
 
   # Powerlineのセットアップ
   if ! has powerline; then
