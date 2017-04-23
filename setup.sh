bcab23cff194061b466d76606c6c575e62993497
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-04-24 01:52:18 +0900

    :sparkles: add git hooks

--- setup.sh
+++ setup.sh
@@ -91,6 +91,7 @@ setup() {
   symlink "$dotfiles/.gitconfig" "$HOME/.gitconfig"
   symlink "$dotfiles/.gitignore_global" "$HOME/.gitignore_global"
   symlink "$dotfiles/.gitmessage" "$HOME/.gitmessage"
+  symlink "$dotfiles/.git_template" "$HOME/"
   /bin/echo -e "<<< [\e[1;32m ok \e[m] "
 
 
